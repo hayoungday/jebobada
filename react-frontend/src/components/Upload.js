3a139163054726c943a51f15f1b0b8216b9f8cba
hayoungday <yha3065@naver.com>
2021-09-28 05:17:17 +0900

    board(1)

--- react-frontend/src/components/Upload.js
+++ react-frontend/src/components/Upload.js
@@ -1,48 +1,77 @@
-import React, {Component} from 'react';
+import React, {Component,useState} from 'react';
 import Header from './Header';
 import axios from 'axios';
-
+import Evidence from './Evidence';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Upload extends Component { 
 
-    state = { 
-        boards: [ 
-            { 
-                brdno: 1, 
-                brdwriter: 'Lee SunSin', 
-                brdtitle: 'If you intend to live then you die', 
-                brddate: new Date() 
-            }, 
-            { 
-                brdno: 2, 
-                brdwriter: 'So SiNo', 
-                brdtitle: 'Founder for two countries', 
-                brddate: new Date() 
-            } 
-        ] 
-    } 
+    // state = { 
+    //     boards: [ 
+    //         { 
+    //             brdno: 1, 
+    //             brdwriter: 'Lee SunSin', 
+    //             brdtitle: 'If you intend to live then you die', 
+    //             brddate: new Date() 
+    //         }, 
+    //         { 
+    //             brdno: 2, 
+    //             brdwriter: 'So SiNo', 
+    //             brdtitle: 'Founder for two countries', 
+    //             brddate: new Date() 
+    //         } 
+    //     ] 
+    // } 
 
-    loadItem = async() => {
-        axios.get("/getuser")
-        .then(res => {
-            console.log(res)
-            console.log(res.data)
-            // console.log(res.data[0])
-            console.log(res.data[0].filename)
-            console.log(typeof res.data)
-            // console.log(res[1].filename)
-        })
+    state = {
+        maxNo: 1,
+        boards:"",
+        completed:0
     }
 
     componentDidMount(){
-        this.loadItem();
+        // this.loadItem();
+        this.timer = setInterval(this.progress,20)
+        this.callApi()
+        .then(res => this.setState({boards:res}))
+        .catch(err => console.log(err))
+    }
+
+    callApi = async() => {
+        const response = await fetch('/getuser')
+        const body = await response.json();
+        console.log(body)
+        return body
+    }
+
+    // loadItem = async() => {
+    //     axios.get("/getuser")
+    //     .then(res => {
+    //         console.log(res)
+    //         console.log(res.data)
+    //         // console.log(res.data[0])
+    //         console.log(res.data[0].filename)
+    //         console.log(typeof res.data)
+    //         // console.log(res[1].filename)
+    //     })
+    // }
+
+    progress = () =>{
+        const { completed } = this.state;
+        this.setState({completed: completed >=100 ? 0 : completed +1})
     }
 
     render() { 
-        const { boards } = this.state; 
-        const list = boards.map(function(row){ 
-            return row.brdno + row.brdwriter ; 
-        }); 
+        // const { boards } = this.state; 
+        // const list = boards.map(function(row){ 
+        //     return row.brdno + row.brdwriter ; 
+        // }); 
         
         return ( 
             <div>
@@ -54,8 +83,29 @@ class Upload extends Component {
                         파일 업로드
                     </label> */}
                     <input type = "submit" />
-                </form> 
-                <table border="1"> 
+                </form>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>피해자명</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { this.state.boards ? this.state.boards.map((c,i) => {
+                            return ( <Evidence key={this.state.maxNo + i} id={this.state.maxNo + i} name={c.filename} user_id={c.user_id} />)
+                            }):
+                            <TableRow>
+                                <TableCell colSpan="6" align="center">
+                                    <CircularProgress variant = "determinate" value={this.state.completed}/>
+                                </TableCell>
+
+                            </TableRow>    
+                        }
+                    </TableBody>
+                </Table>
+                {/* <table border="1"> 
                     <tbody> 
                         <tr align="center"> 
                             <td width="50">No.</td> 
@@ -69,7 +119,7 @@ class Upload extends Component {
                             ) 
                         } 
                     </tbody> 
-                </table>
+                </table> */}
             </div> 
         ); 
     } 
