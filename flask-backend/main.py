49de4e7c268b4cde94ad14d1204a31d291b29b10
hayoungday <yha3065@naver.com>
2021-09-27 15:05:50 +0900

    commit (local)

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -222,5 +222,6 @@ def receive():
     return 'ok'
 
 
-if __name__=='__main__':
- app.run(host='0.0.0.0', port=80, debug=True)
+# if __name__=='__main__':
+#  app.run(host='0.0.0.0', port=80, debug=True)
+app.run(debug=True)
