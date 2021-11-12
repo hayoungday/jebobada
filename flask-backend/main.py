18287ba1470a4b99632d5f794fd1d3daa185c5bd
hayoungday <yha3065@naver.com>
2021-11-12 14:34:52 +0900

    hy deplay2

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -892,5 +892,8 @@ def evidenceupdate():
     return "success"
 
 
+# if __name__=='__main__':
+#  app.run(host='0.0.0.0', port=5000, debug=True)
+
 if __name__=='__main__':
- app.run(host='0.0.0.0', port=5000, debug=True)
+ app.run(host='0.0.0.0', port=80, debug=True)
