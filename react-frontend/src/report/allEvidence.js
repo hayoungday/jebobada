55d249bf1f4bb6e18c22d9fc96b348969f505b4f
hayoungday <yha3065@naver.com>
2021-12-09 03:30:24 +0900

    all evidence table haejoosaeyong

--- react-frontend/src/report/allEvidence.js
+++ react-frontend/src/report/allEvidence.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import AllEvidenceTable from './allEvidenceTable';
 import './reportHeader.css'
 import ReportHeader from './ReportHeader';
+import Stack from "@mui/material/Stack";
 
 const AllEvidence = (props) => {
 
@@ -39,35 +40,47 @@ const AllEvidence = (props) => {
           <div className="nav-item">
             <ReportHeader case_id={props.location.state.case_id}/>
           </div>
-          <div className="comp-item">
-            <h1>전체 자료 목록</h1> 
-              전체 증거 자료 목록입니다.<p/>해시값 비교를 통해 증거 자료의 위변조 여부를 알 수 있습니다.
-                <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell style={{ textAlign: "center" }}>No</TableCell>
-                    <TableCell style={{ textAlign: "center" }}>이름</TableCell>
-                    <TableCell style={{ textAlign: "center" }}>분류</TableCell>
-                    <TableCell style={{ textAlign: "center" }}>*해시값</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                
-                    {evidence.map((c, i) => {
-                      return (
-                        <AllEvidenceTable
-                          index = {no+i}
-                          name={c.filename}
-                          type={c.filetype}
-                          filehash={c.file_hash_data}
-                        />
-                      );
-                    })}
-                </TableBody>
-              </Table>
-              *해시값 : 디지털 증거의 원본성을 입증하기 위해 파일 특성을 축약한 암호같은 수치
-          </div>
-        </div>
+          <div className="yoon_overview-container">
+          <Stack direction="row" alignItems="center" spacing={6}>
+            <span className="yoon_overview-title">전체 자료 목록</span>
+            <br />
+            <span className="yoon_overview-tilte-desc">
+            전체 증거 자료 목록입니다.
+              <br />
+              해시값 비교를 통해 증거 자료의 위변조 여부를 알 수 있습니다.
+            </span>
+          </Stack>
+          <br /><br />
+
+            <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ textAlign: "center" }}>No</TableCell>
+                <TableCell style={{ textAlign: "center" }}>이름</TableCell>
+                <TableCell style={{ textAlign: "center" }}>분류</TableCell>
+                <TableCell style={{ textAlign: "center" }}>*해시값</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            
+                {evidence.map((c, i) => {
+                  return (
+                    <AllEvidenceTable
+                      index = {no+i}
+                      name={c.filename}
+                      type={c.filetype}
+                      filehash={c.file_hash_data}
+                    />
+                  );
+                })}
+            </TableBody>
+          </Table>
+          <br/>
+          <span className="allevdi_contents_desc">
+            *해시값 : 디지털 증거의 원본성을 입증하기 위해 파일 특성을 축약한 암호같은 수치
+          </span>
+      </div>
+    </div>
     )
 }
 
